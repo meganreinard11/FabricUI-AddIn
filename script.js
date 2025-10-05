4dd28cddee27080e71c18046859f663e10331afa
meganreinard11 <35755033+meganreinard11@users.noreply.github.com>
2025-10-05 14:39:16 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See License.txt in the project root.
+*/
+
+// The initialize function must be run each time a new page is loaded
+Office.initialize = function (reason) {
+	$(document).ready(function () {
+		$('#set-data').click(writeText);
+	});
+
+	 //UI Components init
+     $(".ms-Pivot").Pivot();
+     $(".ms-SearchBox").SearchBox();
+     $(".ms-Dropdown").Dropdown();
+     $(".ms-ListItem").ListItem();
+};
+
+// Reads data from current document selection and displays a notification
+function writeText() {
+    Office.context.document.setSelectedDataAsync("Citation goes here",
+        function (asyncResult) {
+            var error = asyncResult.error;
+            if (asyncResult.status === "failed"){
+            	$('#display-data').text("Failure" + error.message);
+            }
+            else
+            {
+            	$('#display-data').text("Done");
+            }
+        });
+}
